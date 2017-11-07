ee3b57bba09b9a4e8ceec6d42cdd7cc613b27b2a
Tamar Zil-Bar <Tamar Zil-Bar>
2017-11-07 11:06:06 +0200

    added heroku DB config

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -1,9 +1,32 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('chatrooms', 'chatrooms', '123456', {
-    host: 'localhost',
-    dialect: 'postgres'
-});
+var sequelize = null;
+if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
+    // Heroku config
+    sequelize = new Sequelize(process.env.DATABASE_URL);
+} else {
+    // local config
+    sequelize = new Sequelize('chatrooms', 'chatrooms', '123456', {
+        host: 'localhost',
+        dialect: 'postgres'
+    });
+}
+
+/*
+ if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
+ // the application is executed on Heroku ... use the postgres database
+ sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL, {
+ dialect:  'postgres',
+ protocol: 'postgres',
+ port:     match[4],
+ host:     match[3],
+ logging:  true //false
+ })
+ } else {
+ // the application is executed on the local machine ... use mysql
+ sequelize = new Sequelize('example-app-db', 'root', null)
+ }
+ */
 
 module.exports = {
     sequelize: sequelize,
